1c5ce36fe8b61d03dd9607278c390e922cfe832c
rauantehsaint <rauanthesaint@gmail.com>
2025-01-27 16:40:26 +0500

    CORS Policy update

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,16 @@
+import cors from 'cors'
 import express from 'express'
 const app = express()
+
+app.use(
+    cors({
+        origin: '*', // Adjust the origin as needed for security
+        credentials: true,
+        methods: 'PUT, POST, GET, DELETE, PATCH, OPTIONS',
+        allowedHeaders: 'Content-Type',
+        maxAge: 1800,
+    })
+)
 
 app.get('/', (req, res) => {
     res.json({
