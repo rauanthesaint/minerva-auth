ca3d69beeeb653082a692ac315b3c30acf40d2e2
rauantehsaint <rauanthesaint@gmail.com>
2025-01-27 16:27:14 +0500

    Server setup

---
+++ api/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hi',
+    })
+})
+
+app.listen(3000, () => console.log('Server ready on port 3000'))
+
+export default app
